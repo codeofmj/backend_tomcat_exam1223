3dd3d67da0c3d5bbd8766a8c9b7092cc8d814626
KimUnBi <kwb5025@gmail.com>
2025-12-30 10:27:53 +0900

    board detail, download func add

--- BoardProject/src/main/java/com/smhrd/web/controller/BoardRestController.java
+++ BoardProject/src/main/java/com/smhrd/web/controller/BoardRestController.java
@@ -3,26 +3,99 @@ package com.smhrd.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.smhrd.web.BoardProjectApplication;
+import com.smhrd.web.dto.BoardRequestDTO;
 import com.smhrd.web.entity.Board;
 import com.smhrd.web.service.BoardService;
 
 @RestController
 @RequestMapping("/api/board")
-@CrossOrigin(origins = "http://10.1.1.6", allowedHeaders = "*")
-//@CrossOrigin(origins = "http://127.0.0.1:5500", allowedHeaders = "*")
+//@CrossOrigin(origins = "http://10.1.1.6", allowedHeaders = "*")
+@CrossOrigin(origins = "http://127.0.0.1:5500", allowedHeaders = "*")
 public class BoardRestController {
+
+    private final BoardProjectApplication boardProjectApplication;
 	
 	@Autowired
 	private BoardService service;
+
+    BoardRestController(BoardProjectApplication boardProjectApplication) {
+        this.boardProjectApplication = boardProjectApplication;
+    }
 
 	@GetMapping("/list")
 	public List<Board> list() {
 		return service.getList();
 	}
+	
+	@PostMapping("/register")
+	public String register(@ModelAttribute BoardRequestDTO dto) {
+		
+		try {
+			
+			service.register(dto);
+			return "success";
+			
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+			return "fail";
+			
+		}
+		
+	}
+	
+	@GetMapping("/{idx}")
+	public Board getDetail(@PathVariable("idx") Long b_idx) {
+		
+		System.out.println("요청한 특정 게시글 글번호:"+b_idx);
+		
+		Board board = service.getDetail(b_idx);
+		
+		System.out.println("조회된 게시글 데이터:"+board);
+		
+		return board;
+	}
+	
+	@GetMapping("/{idx}/download")
+	public ResponseEntity<Resource> download(@PathVariable("idx") Long b_idx) {
+		
+		try {
+			
+			return service.download(b_idx);
+			
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+			return null;
+		}	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
